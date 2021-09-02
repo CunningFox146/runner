02c9f86abf276e7a40b49467235195cb42700838
CunningFox146 <dezhemesov.work@gmail.com>
2021-09-02 15:10:06 +0300

    - CameraManager now changes background color depending on current level template

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -1,4 +1,7 @@
+using DG.Tweening;
 using Runner.Player;
+using Runner.World;
+using Runner.World.LevelTemplates;
 using UnityEngine;
 
 namespace Runner.Managers
@@ -6,10 +9,25 @@ namespace Runner.Managers
     public class CameraManager : Singleton<CameraManager>
     {
         [SerializeField] private PlayerController _player;
+        [SerializeField] private Camera _camera;
         [SerializeField] private Vector3 _offset;
         [SerializeField] private float _moveSpeed;
 
         public bool isFollowing = true;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _camera = GetComponent<Camera>();
+
+            LevelGenerator.OnTemplateChanged += OnTemplateChangedHandler;
+        }
+
+        private void OnTemplateChangedHandler(LevelTemplate oldTemplate, LevelTemplate newTemplate)
+        {
+            _camera.DOColor(newTemplate.skyColor, 1f).SetEase(Ease.InCubic);
+        }
 
         void Start()
         {
