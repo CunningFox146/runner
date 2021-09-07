600d4201ec2db8089af7830c11033efd527e2066
CunningFox146 <dezhemesov.work@gmail.com>
2021-09-07 20:02:53 +0300

    - Added coin generator - Added coin generator edditor

---
+++ Assets/Editor/CoinGeneratorEditor.cs
@@ -0,0 +1,27 @@
+﻿using Runner.World;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+    [CustomEditor(typeof(CoinGenerator))]
+    public class CoinGeneratorEditor : UnityEditor.Editor
+    {
+        private CoinGenerator _target;
+
+        public void OnEnable()
+        {
+            _target = (CoinGenerator)target;
+        }
+
+        public void OnSceneGUI()
+        {
+            float size = 0.5f;
+            Handles.color = Color.red;
+            foreach (Vector3 pos in _target.GetSpawnPositions())
+            {
+                Handles.DrawWireDisc(_target.transform.position + pos + Vector3.up * size, Vector3.forward, size, 5f);
+            }
+        }
+    }
+}
