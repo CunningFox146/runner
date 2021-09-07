6e6985c57dbc0aecc35f35b40d15c5c8401953d5
CunningFox146 <dezhemesov.work@gmail.com>
2021-09-07 19:36:29 +0300

    - Added coin

---
+++ Assets/Scripts/Interactable/Coin.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+
+namespace Runner.Interactable
+{
+    public class Coin : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private Transform _model;
+
+        void Start()
+        {
+            transform.DORotate(new Vector3(0f, -360f, 0f), 1f, RotateMode.FastBeyond360)
+                .SetEase(Ease.Linear)
+                .SetLoops(-1);
+        }
+
+        public void OnInteractStart(GameObject player)
+        {
+            Destroy(gameObject);
+        }
+
+        public void OnInteractStop(GameObject player) {}
+    }
+}
