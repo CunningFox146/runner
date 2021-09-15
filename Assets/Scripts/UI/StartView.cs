c293954ae025f219ca4f0f2252b9307df973a5a2
CunningFox146 <makar.papca@gmail.com>
2021-09-15 15:09:59 +0300

    - Added title and main fonts - Added shop button - Added title - Added click handling to start the game

--- Assets/Scripts/UI/StartView.cs
+++ Assets/Scripts/UI/StartView.cs
@@ -5,13 +5,6 @@ namespace Runner.UI
 {
     public class StartView : View
     {
-        private void Update()
-        {
-            if (Input.anyKey)
-            {
-                ViewManager.HideView<StartView>();
-                GameManager.StartSession();
-            }
-        }
+        
     }
 }
